8737e2a7930dfbcbf5db1c19f25522f5e2d69c0d
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-05-25 09:25:42 +0100

    add directives

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { TodosService } from '../services/todos.service';
+import { Todo } from '../model/todo.type';
+import { catchError } from 'rxjs';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css'
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit{
 
+  todoService = inject(TodosService);
+  todoItems = signal<Array<Todo>>([]);
+
+  ngOnInit(): void {
+    this.todoService.getTodosFromApi()
+    .pipe(catchError((err)=>{
+      console.log(err);
+      throw err;
+    })
+  ).subscribe((todos)=>{
+    this.todoItems.set(todos);
+  })
+
+  }
 }
