0d2919c4ba41a9eccb2c5ef1ae12e28856ede19f
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-05-26 10:29:31 +0100

    finalize on directives

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,28 +2,44 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../services/todos.service';
 import { Todo } from '../model/todo.type';
 import { catchError } from 'rxjs';
-import { NgIf } from '@angular/common';
+import { TodoItemComponent } from '../components/todo-item/todo-item.component';
 
 @Component({
   selector: 'app-todos',
-  imports: [NgIf],
+  imports: [TodoItemComponent],
   templateUrl: './todos.component.html',
-  styleUrl: './todos.component.css'
+  styleUrl: './todos.component.css',
 })
-export class TodosComponent implements OnInit{
-
+export class TodosComponent implements OnInit {
   todoService = inject(TodosService);
   todoItems = signal<Array<Todo>>([]);
 
   ngOnInit(): void {
-    this.todoService.getTodosFromApi()
-    .pipe(catchError((err)=>{
-      console.log(err);
-      throw err;
-    })
-  ).subscribe((todos)=>{
-    this.todoItems.set(todos);
-  })
+    this.todoService
+      .getTodosFromApi()
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          throw err;
+        })
+      )
+      .subscribe((todos) => {
+        this.todoItems.set(todos);
+      });
+  }
 
+  updateTodoItem(todoItem: Todo){
+    this.todoItems.update((todos)=> {
+      return todos.map(todo=>{
+        if(todo.id==todoItem.id){
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+        return todo; 
+      })
+    })
+    
   }
 }
